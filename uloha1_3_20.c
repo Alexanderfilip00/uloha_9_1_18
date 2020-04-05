897a16b4ae1430e9b8fa2fc066d1ceb60665d06d
Alexander Filip <alexanderfilip00@gmail.com>
2020-04-05 13:10:05 +0200

    druhy pokus

--- uloha1_3_20.c
+++ uloha1_3_20.c
@@ -1,6 +1,7 @@
 	#include <stdio.h>
 	#include <stdlib.h>
-	#define n 10		//sem zadam sam hodnotu
+	#include <math.h>
+	#define n 40		//sem zadam sam hodnotu
 	
 	void Napln(int p[], int d){
 		int i;
@@ -10,29 +11,34 @@
 	}
 	
  char nahodna_permutacia_s_obmedzenim_vzdialenosti(int *p, int l, int dist){
- 	int helpole[n]={0};
- 	int i,temp_a,temp_zaciatok,temp_koniec;
+ 	int helpole[n]={-1};
+ 	int i,temp_a,temp_zaciatok,temp_koniec,abs_pocitadlo,polovica_n;
+ 	int pocitadlo=-1;
  	
  	for(i=p;i<=p+n;i+4){		//i+4, pretoze sa posuvam po 4 bytoch kvoli integerom
- 		if (i-p)<dist{
- 			temp_a = rand()%((2*dist)+1)-(i-p);
-		 	}
+ 		
+		if (n%2==0){
+			polovica_n= n/2;
+		} 
 		else{
-			temp_a = rand()%((2*dist)+1)-dist;	
+			polovica_n= n+1/2;
 		}
 		
-	if (helpole[temp_a]==0){
-		helpole[temp_a]=temp_a;
-	}
-	else if(temp_a==(i-dist*4))	
-		
-		
-	}
- 	
- 	for (i=0;i<20;i++){
- 		printf("%d ",rand()%(2*dist+1)-dist);
-	}
-				
+ 		++pocitadlo;
+ 		abs_pocitadlo = polovica_n - abs(n/2 - pocitadlo) ;		//abs_pocitadlo rata vzdialenost od kraja... ak to je koniec, ma zaporne hodnoty
+ 		if (pocitadlo>n/2)	abs_pocitadlo*= -1;
+ 		if (dist>=n || pocitadlo>=dist)	abs_pocitadlo=0;				//ak by sa hodnoty mohli posuvat po celom poli, nerob zmeny
+ 		
+ 		temp_a = rand()%((2*dist - (dist-pocitadlo) )+1)-(pocitadlo);
+ 		printf("%d . hodnota: %d - patri medzi zaciatocne. \n",pocitadlo,temp_a);
+ 			
+ 		if (helpole[temp_a]==-1){
+			helpole[temp_a]=temp_a;
+			}
+		else if(temp_a==(i-(i-p))){
+			}	
+ 			
+		 	}
 
  }
 	
