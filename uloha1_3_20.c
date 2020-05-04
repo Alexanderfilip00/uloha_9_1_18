a825d99ceddf79256122a50454858b11d5eb7eb1
Alexander Filip <alexanderfilip00@gmail.com>
2020-05-04 21:36:07 +0200

    upravy podla napomenutia

--- uloha1_3_20.c
+++ uloha1_3_20.c
@@ -1,47 +1,49 @@
 	#include <stdio.h>
 	#include <stdlib.h>
 	#include <math.h>
-	#define n 50
 	
 	void Napln(int p[], int d){
 		int i;
 		puts("Generujem zaciatocne poradie:");
-		for(i=0;i<=d;i++){
+		for(i=0;i<d;i++){
 			p[i]=rand()%100;			//generacia nahodnych cisel [0,99], aby som tam nemal prilis velke hodnoty
-			printf("%d ", p[i]);	
+			printf("%2d ", p[i]);	
 		}
 		printf("\n \n");
 	}
 	
- char nahodna_permutacia_s_obmedzenim_vzdialenosti(int *p, int l[], int dist){
+ char nahodna_permutacia_s_obmedzenim_vzdialenosti(int *p, int l, int dist){
  	
-	int i,j;	
- 	int helppole[n]={0};
- 	int pole_zoradenie[n]={0};
+	int i,j;
+	int *helppole = malloc(sizeof(int)*l);
+ 	int *pole_zoradenie = malloc(sizeof(int)*l);
+ 	
+ 	for(i=0;i<l;i++){
+ 		helppole[i]=0;
+ 		pole_zoradenie[i]=0;
+	 }
 	int pokracuj=0, done=0;
 	int min, max, odzadu=0;	
 	int gen;
 
-	Napln(p,n);			//naplnim povodne pole nahodnymi cislami
+	Napln(p,l);			//naplnim povodne pole nahodnymi cislami
 	
-	if(dist>n) dist=n;
+	if(dist>l) dist=l;
  	
  	while (done==0){
  		
-	 	for(i=0;i<n;i++){			//prechadzam po novom poli, kde bude presuvanie
+	 	for(i=0;i<l;i++){			//prechadzam po novom poli, kde bude presuvanie
 			
 	 		odzadu++;
-			if (i==n-1) {
+			if (i==l-1) {
 				done=1; 
 			}
 			
 			pokracuj=0;
-	 		
-			//printf("Zacinam %d\n", i);			//
 			
 	 		if(i-dist<=0) min=0;
 	 		else min= i-dist;
-	 		if(i+dist>=n) max=n;
+	 		if(i+dist>=l) max=l;
 	 		else max= i+dist;
 	 		
 	 		while(pokracuj==0){
@@ -67,12 +69,13 @@
 				 	min++;
 				 	}
 				if(max-min == 0) {						//ak dojdu moznosti, resetuj sa
-					//for(j=0;j<n;j++)	printf("%d ",helppole[j]);				
+					//for(j=0;j<l;j++)	printf("%d ",helppole[j]);				
 					
 					//printf("\n\t\t\t\t Podarilo sa: %d \n",i);			//
-					for(j=0;j<n;j++) helppole[j]=0;
+					for(j=0;j<l;j++) helppole[j]=0;
 					odzadu=-1;
 					pokracuj=0;
+					done=0;
 					i=0; 
 					//puts("\t\t\tUplne odznova");					//
 					break;
@@ -81,9 +84,9 @@
 	 		}
 	 		
 	 		if(i>dist+1 && helppole[i-dist-1]==0){				//kontrola dozadu - ci to nerata zbytocne
-				//for(j=0;j<n;j++)	printf("%d ",helppole[j]);				
+				//for(j=0;j<l;j++)	printf("%d ",helppole[j]);				
 				//printf("\n\t\t\t\t Podarilo sa: %d \n",i);			//
-				for(j=0;j<n;j++) helppole[j]=0;
+				for(j=0;j<l;j++) helppole[j]=0;
 				odzadu=-1;
 				pokracuj=0;
 				i=0; 
@@ -94,27 +97,32 @@
  	}
 	
 	puts("Nove poradie:");
-	for (i=0;i<n;i++){
-		printf("%d ", pole_zoradenie[i]);			//pre kontrolu mi vypise, ake nastanu posuny
-		helppole[i]=l[i];							//skopiruje povodne pole - priprava na permutaciu
+	for (i=0;i<l;i++){
+		printf("%2d ", pole_zoradenie[i]);			//pre kontrolu mi vypise, ake nastanu posuny
+		helppole[i]=p[i];							//skopiruje povodne pole - priprava na permutaciu
 	}
 	
 	
-	for(i=0;i<n;i++){
-		p[pole_zoradenie[i]-1]=helppole[i];
+	for(i=0;i<l;i++){
+		p[pole_zoradenie[i]]=helppole[i];
 	}
 	puts("");
 	puts("Nova permutacia:");
-	for(i=0;i<n;i++){
-		printf("%d ",p[i]);
+	for(i=0;i<l;i++){
+		printf("%2d ",p[i]);
 	}
 	
  }
 	
 	int main(){
 		srand(time(NULL));
-		int dist=5;		//sem zadam sam hodnotu  
-		int l[n]={-1};
-		int *p=l;	
-		nahodna_permutacia_s_obmedzenim_vzdialenosti(p, l, dist);
+		int i;
+		int dist=6;		//sem zadam sam hodnotu  
+		int n=50;
+		int *pole = malloc(sizeof(int)*n);
+		for(i=0;i<n;i++) pole[i]=-1;
+		
+		//int pole[n]={-1};
+		int *p = pole;	
+		nahodna_permutacia_s_obmedzenim_vzdialenosti(p, n, dist);
 	}
