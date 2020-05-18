d0e5216a533159dcfde59d41d0862dc0b8d62d60
Alexander Filip <alexanderfilip00@gmail.com>
2020-05-18 15:37:19 +0200

    Praca na zakladnych funkciach

--- uloha9_1_18.c
+++ uloha9_1_18.c
@@ -1,77 +1,143 @@
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <math.h>
-	#include <fcntl.h>
-	#include <string.h>
-	#include <sys/stat.h>
-	#include <unistd.h>
-	#define ELEM(M,r,c) (M->elem[(M->cols)*r+c])
-	#ifndef O_BINARY
-	#define O_BINARY 0
-	#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define ELEM(M,r,c) (M->elem[(M->cols)*r+c])
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif
+
+typedef struct{
+	unsigned int rows;
+	unsigned int cols;
+	float *elem;
+	}MAT;
+
+MAT *mat_create_with_type(unsigned int rows, unsigned int cols){
 	
-	typedef struct{
-		unsigned int rows;
-		unsigned int cols;
-		float *elem;
-		}MAT;
+	MAT *A;
+	A = (MAT*)malloc(sizeof(MAT));
 	
-	MAT *mat_create_with_type(unsigned int rows, unsigned int cols){
-		
-		MAT *A;
-		A = (MAT*)malloc(sizeof(MAT));
-		
-		float *pole;
-		pole = (float*)malloc(sizeof(float)*rows*cols);
-		
-		A->rows = rows;
-		A->cols = cols;
-		A->elem = pole;
-		
-		if(A==NULL){
-			free(A);
-			free(pole);
-			return NULL;
-		}
-		return A;	
+	float *pole;
+	pole = (float*)malloc(sizeof(float)*rows*cols);
+	
+	A->rows = rows;
+	A->cols = cols;
+	A->elem = pole;
+	
+	if(A==NULL){
+		free(A);
+		free(pole);
+		return NULL;
+	}
+	return A;	
+}
+
+MAT *mat_create_by_file(char *filename){
+	
+	unsigned int r,s;
+	int dlzka;
+	char typ[2]={0};
+	int FILE = open(filename,O_RDONLY | O_BINARY, S_IRUSR);
+	if( FILE < 0){
+		printf("Subor neexistuje.");
+		return NULL;
 	}
 	
-	MAT *mat_create_by_file(char *filename){
-	
-	}
-	
-	char mat_save(MAT *mat, char *filename){
-		
-		int i;
-		int FILE = open(filename,O_BINARY | O_CREAT, S_IWUSR);
-		if( FILE < 0){
-			return 0;
-		}
-		
-		char buf = 'm';
-		write(FILE, &buf, 4);
-		write(FILE, "1", sizeof(char));
-		write(FILE, &mat->rows, sizeof(unsigned int));
-		write(FILE, &mat->cols, sizeof(unsigned int));
-		printf("%c",buf);
-		write(FILE, &mat->elem,sizeof(float)*mat->rows*mat->cols);
-		
-		
-		close(FILE);	
-	}
-	
-	void mat_destroy(MAT *mat){
-		free(&mat->elem);
-		free(mat);
-		
+	read(FILE, &typ, sizeof(char)*2);
+	if(typ[0] != 'M' || typ[1] != '1'){
+		printf("\nCHYBA: Subor nereprezentuje hustu maticu.\n");
+		return NULL;
 	}
 
-	
-	int main(){
-		MAT *A = mat_create_with_type(5,5);
-		//printf("%2.1f\n", ELEM(A,0,0));
-		//printf("%2.1f ",A->elem[0]);
-		mat_save(A, "skuska2.bin");
-		
-		
+	read(FILE, &r, sizeof(unsigned int));
+	read(FILE, &s, sizeof(unsigned int));
+
+	if(r==EOF || s==EOF){
+		printf("\nCHYBA: Koniec suboru.\n");		
+		return NULL;
 	}
+	MAT *A = mat_create_with_type(r,s);
+
+	dlzka = read(FILE, A->elem, sizeof(float)*r*s);
+	if (dlzka != sizeof(float)*r*s){
+		printf("\nCHYBA: Nedostatok hodnot.\n");
+		return NULL;
+	}
+	
+	close(FILE);
+	return A;
+}
+
+
+void mat_unit(MAT *mat){
+	int i,j;
+	for(i=0 ; i < mat->rows ; i++){
+		for(j=0 ; j < mat->cols ; j++){
+			if(i != j)
+				ELEM(mat,i,j)=0.0;
+			else
+				ELEM(mat,i,j)=1.0;
+		}
+	}
+}
+
+char mat_save(MAT *mat, char *filename){
+	
+	int FILE = open(filename,O_CREAT | O_WRONLY | O_BINARY, S_IRUSR);
+	if( FILE < 0){
+		return 0;
+	}
+	
+	write(FILE, "M", sizeof(char));
+	write(FILE, "1", sizeof(char));
+	write(FILE, &mat->rows, sizeof(unsigned int));
+	write(FILE, &mat->cols, sizeof(unsigned int));
+	write(FILE, &mat->elem, sizeof(float)*(mat->rows)*(mat->cols));
+	close(FILE);	
+}
+
+
+void mat_destroy(MAT *mat){
+	free(&mat->elem);
+	free(mat);
+}
+
+void mat_print(MAT *mat){
+	int i,j;
+	for(i=0 ; i < mat->rows ; i++){
+			for(j=0 ; j < mat->cols ; j++){
+					printf("%5.2f ",ELEM(mat,i,j));
+			}
+		printf("\n");
+		}
+}
+
+void mat_random(MAT *mat){
+	int i,j;
+	for(i=0 ; i < mat->rows ; i++){
+		for(j=0 ; j < mat->cols ; j++){
+			ELEM(mat,i,j)=(rand() / 32767.0)*2.0 - 1.0;	
+		}
+	}
+
+}
+
+//...............................................................................................
+
+int main(){
+	srand(time(NULL));
+	MAT *A = mat_create_with_type(7,7);
+	
+	mat_random(A);
+	mat_save(A, "skuska1.bin");
+	mat_print(A);
+	printf("\n");
+	MAT *B = mat_create_by_file("skuska1.bin");
+	mat_print(B);
+	
+	
+}
